64ce2ffbc367ac58226822483a89c15e02cf90e4
RazzMaDazzle <datrollanator@gmail.com>
2025-01-09 12:13:00 -0800

    Adding iteration over file

--- linecount.py
+++ linecount.py
@@ -1,11 +1,20 @@
+#Control and file variables
+count = 0
 condition = True
 fname = input(f"Please enter a file name to count: ")
 
+#Input handling 
 while(condition):
     try:
         f = open(fname, "r")
         condition = False
     except Exception:
         fname = input(f"The name caused an error, {Exception}, please try again: ")
-print("The file opened succesfully")
+
+#Line iteration
+for i in f:
+    count += 1
+
+print(f"The number of lines in the file is {count}")
+
 f.close()
